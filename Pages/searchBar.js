a4bad6e073994f1600a59184ab1a8550ccf0d8e9
Jake1585 <76916765+Jake1585@users.noreply.github.com>
2023-04-24 22:49:34 +0300

    Update searchBar.js

--- Pages/searchBar.js
+++ Pages/searchBar.js
@@ -7,10 +7,7 @@ function findWords()
     ///loops through the array to get each word
     for( var i=0; i<words.length; i++)
     {      
-        //This is where I get lost, i dont know how to make it so that what you enter
-        //in the textbox(lookfor) matches somthing on the page. it will get highlighted. i
-        //was thinking about using the find object, but im new to javascript and dont know 
-        //how to properly use find yet.
+
         if(searchHeader == words[i]) //Find()------
         { 
             '<span class="highlight">' + words + '</span>'
